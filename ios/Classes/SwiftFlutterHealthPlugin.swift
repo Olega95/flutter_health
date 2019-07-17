8a77fb7bcdcf124ba866ba52be5e8fc06339a895
bulenthacioglu <bulent.hacioglu@hotmail.com>
2019-07-17 18:50:33 +0200

    apple health kind of works

--- ios/Classes/SwiftFlutterHealthPlugin.swift
+++ ios/Classes/SwiftFlutterHealthPlugin.swift
@@ -1,7 +1,11 @@
 import Flutter
 import UIKit
+import HealthKit
 
 public class SwiftFlutterHealthPlugin: NSObject, FlutterPlugin {
+    
+    let healthStore = HKHealthStore()
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_health", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterHealthPlugin()
@@ -9,6 +13,27 @@ public class SwiftFlutterHealthPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    if(call.method.elementsEqual("checkIfHealthDataAvailable")){
+        if HKHealthStore.isHealthDataAvailable() {
+            result("Matomo:: HealthKit available.")
+        }
+    } else if(call.method.elementsEqual("requestAuthorization")){
+        let allTypes = Set([HKObjectType.workoutType(),
+                            HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                            HKObjectType.quantityType(forIdentifier: .stepCount)!,
+                            HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
+                            HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                            HKObjectType.quantityType(forIdentifier: .heartRate)!])
+        
+        healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+            if !success {
+                result("Error!!!")// Handle the error here.
+            } else{
+                result("Success!!!")
+            }
+        }
+    }
   }
 }
+
+
--- lib/flutter_health.dart
+++ lib/flutter_health.dart
@@ -1,13 +1,23 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class FlutterHealth {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_health');
+  static const MethodChannel _channel = const MethodChannel('flutter_health');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  static Future<String> checkIfHealthDataAvailable(BuildContext context) async {
+    final String version = await _channel.invokeMethod('checkIfHealthDataAvailable');
+    return version;
+  }
+
+  static Future<String> requestAuthorization(BuildContext context) async {
+    final String version = await _channel.invokeMethod('requestAuthorization');
     return version;
   }
 }
